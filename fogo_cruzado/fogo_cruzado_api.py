afdbcddd34edd0e63e9f5f9a7c9087f3bb87bc83
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2024-09-14 13:51:39 -0300

    Add Fogo Cruzado API

---
+++ fogo_cruzado/fogo_cruzado_api.py
@@ -0,0 +1,112 @@
+import requests
+from datetime import datetime
+
+
+class DateHelper:
+    
+    @staticmethod
+    def get_now_timestamp():
+        now = datetime.now()
+        timestamp = datetime.timestamp(now)
+        return timestamp
+
+
+class Credentials:
+    email: str
+    password: str
+    
+    def __init__(self, email: str, password: str) -> None:
+        self.email = email
+        self.password = password
+    
+
+class FogoCruzadoApi:
+    
+    token: str = None
+    expiration: int = 0
+    
+    def __init__(self, credentials: Credentials) -> None:
+        self.credentials = credentials
+        self.api_base_url = "https://api-service.fogocruzado.org.br/api/v2"
+        self.api_base_headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+        
+    def add_bearer_token_to_headers(self, headers: dict, token: str) -> dict:
+        headers["Authorization"] = f"Bearer {token}"
+        return headers
+    
+    def is_authenticated(self):
+        timestamp = DateHelper.get_now_timestamp()
+        return self.token is not None and timestamp < self.expiration
+
+    
+    def authenticate(self) -> str:
+        response = requests.post(f"{self.api_base_url}/auth/login", json={
+            "email": self.credentials.email,
+            "password": self.credentials.password,
+        }, headers=self.api_base_headers)
+        if response.status_code != 201:
+            print(response.status_code)
+            print(response.text)
+            return None
+        
+        data = response.json()
+        self.token = data['data']['accessToken']
+        self.expiration = DateHelper.get_now_timestamp() + data['data']['expiresIn']
+        return self.token
+    
+    def refresh_access_token(self) -> str:
+        headers = self.add_bearer_token_to_headers(self.api_base_headers, self.token)
+        
+        response = requests.post(f"{self.api_base_url}/auth/refresh", headers=headers)
+        if response.status_code != 201:
+            print(response.status_code)
+            print(response.text)
+        
+        data = response.json()
+        return data['data']['accessToken']
+    
+    def get_states(self) -> list[dict]:
+        if not self.is_authenticated():
+            self.authenticate()
+        
+        headers = self.add_bearer_token_to_headers(self.api_base_headers, self.token)
+        response = requests.get(f"{self.api_base_url}/states", headers=headers)
+        if response.status_code != 200:
+            print(response.status_code)
+            print(response.text)
+            return []
+        
+        data = response.json()
+        return data['data']
+    
+    def get_cities(self) -> list[dict]:
+        if not self.is_authenticated():
+            self.authenticate()
+            
+        headers = self.add_bearer_token_to_headers(self.api_base_headers, self.token)
+        response = requests.get(f"{self.api_base_url}/cities", headers=headers)
+        if response.status_code != 200:
+            print(response.status_code)
+            print(response.text)
+            return []
+        
+        data = response.json()
+        return data['data']
+    
+    def get_occurrences(self, state_id: str) -> list[dict]:
+        if not self.is_authenticated():
+            self.authenticate()
+            
+        headers = self.add_bearer_token_to_headers(self.api_base_headers, self.token)
+        response = requests.get(f"{self.api_base_url}/occurrences?idState={state_id}", headers=headers)
+        if response.status_code != 200:
+            print(response.status_code)
+            print(response.text)
+            return []
+        
+        data = response.json()
+        return data['data']    
+    
