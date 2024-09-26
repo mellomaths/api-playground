b2338e67ab08c80741d6e327a8e582ff7f70e231
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2024-09-25 23:45:31 -0300

    Add homepage

--- Dashboard.py
+++ Dashboard.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 pages = {
+    "Home": [
+        st.Page("pages/home/home.py", title="API Playground", icon="🏠"),
+    ],
     "Fogo Cruzado API": [
         st.Page("pages/fogo_cruzado/fogo_cruzado.py", title="Fogo Cruzado API", icon="🔫"),
     ],
---
+++ pages/home/home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="API Playground", page_icon="🏠", layout="centered")
+
+st.write(f"# API Playground")
+
+st.write(
+"""
+API Playground is a Data Science exploration app developed using the [Streamlit](https://streamlit.io/) library. The idea is to develop my skills in the Data Science world. This app explores real-world data from public APIs and basically the idea is to build many different dashboards that you can checkout in the sidebar on the left.
+
+## Authors
+
+- [Matheus Mello de Lima](https://mellomaths.com/)
+
+This app is deployed under the [Streamlit Community Cloud](https://docs.streamlit.io/deploy/streamlit-community-cloud).
+
+""")
