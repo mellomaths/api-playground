f91d5932d3fb9de3a19c3626ef27975751011266
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2024-09-27 02:09:34 -0300

    Adds url path to pages

--- src/Dashboard.py
+++ src/Dashboard.py
@@ -5,13 +5,13 @@ load_dotenv()
 
 pages = {
     "Home": [
-        st.Page("apps/home/page.py", title="API Playground", icon="🏠"),
+        st.Page("apps/home/page.py", title="API Playground", icon="🏠", url_path="/"),
     ],
     "Fogo Cruzado API": [
-        st.Page("apps/fogo_cruzado/page.py", title="Fogo Cruzado", icon="🔫"),
+        st.Page("apps/fogo_cruzado/page.py", title="Fogo Cruzado", icon="🔫", url_path="/fogo-cruzado"),
     ],
     "Yahoo Finance API": [
-        st.Page("apps/yahoo_finance/page.py", title="Yahoo Finance", icon="💸")
+        st.Page("apps/yahoo_finance/page.py", title="Yahoo Finance", icon="💸", url_path='/yahoo-finance')
     ]
 }
 
